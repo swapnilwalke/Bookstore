852e3f1b2f5de5ff12ea897abaf519f6f19a06e9
Swapnil Walke <pkswalke@gmail.com>
2018-08-26 10:00:33 +0530

    Account Page

---
+++ src/main/java/com/bookstore/domain/User.java
@@ -0,0 +1,12 @@
+package com.bookstore.domain;
+
+public class User {
+	private Long id;
+	private String userName;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phone;
+	private boolean enabled = true;
+}
